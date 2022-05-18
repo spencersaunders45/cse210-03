1541497580d52c10ad9963dbb6a43b3b7bf60957
eagleye123 <49323274+eagleye123@users.noreply.github.com>
2022-05-18 14:57:47 -0600

    added string

---
+++ word.cs
@@ -0,0 +1,17 @@
+namespace cse210_03{
+    class Word{
+        List<string> _words;
+        private void word(){
+            _words.Add("qeue");
+            _words.Add("Squirel");
+            _words.Add("Misspelled");
+            _words.Add("My name is john.");
+            _words.Add("hello there");
+            _words.Add("general kenobi");
+            _words.Add("i have become self aware");
+            _words.Add("whats up dog, my name is jack");
+            _words.Add("luke i am your father");
+            _words.Add("hi");
+        }
+    }
+}
