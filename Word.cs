76596361e9447d05688c06e5d447e541877745ad
eagleye123 <49323274+eagleye123@users.noreply.github.com>
2022-05-23 19:15:51 -0600

    Added return to GetNextWord

--- Word.cs
+++ Word.cs
@@ -19,43 +19,47 @@ public class Word{
 
         theword = "null";
     }
-    //  gets a random word when you call the funciton
-    public void GetNextWord(){
+    public string GetNextWord(){
         Random rd = new Random();
-        // Call random int from size of list size.
         int _current_word = rd.Next(0,(_words.Count -1));
         theword = _words[_current_word];
-
+        return _words[_current_word];
     }
 //    takes in Guess as a one letter string variable, and output a string that tells the user what they got wrong/ right in the guess.
 //    Example: Word hello guess h output h_____ 
-    public void CompareWords( string guess){
-        Console.WriteLine(theword);
+    public string CompareWords( string guess){
+        string wordCurrentState = "";
+        // Console.WriteLine(theword);
         // Goes through each of the letters of the words. It then produces word as characters in the word.
         foreach (var word in theword){
             _currentguessedletter = false;
             // Goes through each of the different letters that the user has already guessed.
             foreach (var guessed in _alreadyguest){
                 if (Convert.ToString(guessed) == Convert.ToString(word)){
-                    Console.Write(guessed);
+                    wordCurrentState = wordCurrentState + guessed;
+                    // Console.Write(guessed);
                     _currentguessedletter = true;
                 }
             }
             //  If you have guessed the letter it will print it out on the screen and then adds it to a list of guessed words.
             if (guess.Equals(Convert.ToString(word)) && _currentguessedletter != true){
-                Console.Write(word);
+                wordCurrentState = wordCurrentState + word;
+                // Console.Write(word);
                 _alreadyguest.Add(Convert.ToString(word));
                 }
             // If there is a space in the words, it will add a space in the words.
             if(string.IsNullOrWhiteSpace(Convert.ToString(word))){
-                Console.Write(" ");
+                wordCurrentState = wordCurrentState + " ";
+                // Console.Write(" ");
             }
             // If the guess is wrong, and 
             else if (_currentguessedletter != true){
-                Console.Write("_");
+                wordCurrentState = wordCurrentState + "_";
+                // Console.Write("_");
 
             }
         }
+        return wordCurrentState;
         }
     }
 }
