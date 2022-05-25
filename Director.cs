2968df470fabbef57b2dcc13ae56d526dd38b698
Junwoo9025 <91073913+Junwoo9025@users.noreply.github.com>
2022-05-25 11:05:13 -0600

    added Parachute class

--- Director.cs
+++ Director.cs
@@ -3,12 +3,13 @@
     public class Director
     {
         // Initialize necessary variables
+        private Parachute _parachute;
         private IO _io;
         private Word _word;
 
         private string _currentWord;
 
-        private int _wrongGuesses;
+        
         private string _guess;
         private string _currentWordState;
 
@@ -17,10 +18,10 @@
         /// </summary>
         public Director()
         {
+            _parachute = new Parachute();
             _io = new IO();
             _word = new Word();
             _currentWord = _word.GetNextWord();
-            _wrongGuesses = 0;
             _currentWordState = "";
 
             // foreach (char character in _currentWord)
@@ -37,10 +38,10 @@
         /// </summary>
         public void RunGame()
         {
-            while (_wrongGuesses < 4)
+            while (_parachute.GetWrongGuesses() < 4)
             {
                 // Display basic information to player
-                _io.DisplayJumper(_wrongGuesses, 4);
+                _io.DisplayJumper(_parachute.GetWrongGuesses(), 4);
                 _io.DisplayCurrentWord(_currentWordState);
                 
                 // Get the players' guess
@@ -51,7 +52,7 @@
                 
                 // Apply the consequence based on the results of the guess comparison
                 if (_currentWordState == guessed)
-                    _wrongGuesses++;
+                    _parachute.IncrementWrongGuesse();
                 else
                     _currentWordState = guessed;
 
@@ -64,7 +65,7 @@
             }
             
             // Loss Condition
-            _io.DisplayJumper(_wrongGuesses, 4);
+            _io.DisplayJumper(_parachute.GetWrongGuesses(), 4);
             Console.WriteLine("You Lose!");
         }
     }
